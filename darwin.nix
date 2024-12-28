77df4157cafeb1ca56ba9761dadb67a07351cbd0
Mårten Pettersson <mtnptrsn@gmail.com>
2024-12-28 10:40:39 +0100

    iter

--- darwin.nix
+++ darwin.nix
@@ -1,14 +1,34 @@
-{ nixpkgs, services, system, ... }:
+# darwin.nix
+
+{ pkgs, ... }:
 
 {
-  nixpkgs.config.allowUnfree = true;
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [ pkgs.vim ];
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # services.karabiner-elements.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Necessary for using flakes on this system.
+  nix.settings.experimental-features = "nix-command flakes";
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true; # default shell on catalina
+  # programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+
+  # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";
 
-  services.nix-daemon.enable = true;
-
-  # system.configurationRevision = self.rev or self.dirtyRev or null;
-  system.stateVersion = 5;
-  system.defaults = {
-    dock.autohide = true;
+  users.users.mtnptrsn = {
+    name = "mtnptrsn";
+    home = "/Users/mtnptrsn";
   };
 }
--- flake.nix
+++ flake.nix
@@ -1,53 +1,27 @@
+# flake.nix
+
 {
-  description = "mtnptrsn";
+  description = "Darwin configuration";
 
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/nixos-unstable";
-    };
-
-    # nixpkgsDarwin = {
-    #   url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
-    # };
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      # Assume we use nixpkgs from Linux for Home Manager purposes across systems
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nix-darwin = {
-      url = "github:LnL7/nix-darwin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, nix-darwin, ... }:
-    let
-      linuxSystem = "x86_64-linux";
-      darwinSystem = "aarch64-darwin";
-      pkgs = nixpkgs.legacyPackages.${linuxSystem};
-    in
-    {
-      homeConfigurations."mtnptrsn-linux" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [ ./home-manager/home.nix ];
-      };
-
-      # homeConfigurations."mtnptrsn-darwin" = home-manager.lib.homeManagerConfiguration {
-      #   inherit pkgsDarwin;
-      #   modules = [ ./home-manager/home.nix ];
-      # };
-
-      darwinConfigurations."mtnptrsn-darwin" = nix-darwin.lib.darwinSystem {
-        # inherit pkgsDarwin;
+  outputs = inputs@{ nixpkgs, home-manager, darwin, ... }: {
+    darwinConfigurations = {
+      hostname = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
         modules = [
           ./darwin.nix
           home-manager.darwinModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.mtnptrsn = import ./home-manager/home.nix;
+            home-manager.users.mtnptrsn = import ./home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass
             # arguments to home.nix
@@ -55,4 +29,5 @@
         ];
       };
     };
+  };
 }
