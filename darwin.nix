68c5d6b8404820e5659771310dc0969700fd1b67
Mårten Pettersson <mtnptrsn@gmail.com>
2024-12-28 11:17:34 +0100

    iter

--- darwin.nix
+++ darwin.nix
@@ -3,15 +3,14 @@
 { pkgs, system, ... }:
 
 {
+  # users.users.mtnptrsn = {
+  #   name = "mtnptrsn";
+  #   home = "/Users/mtnptrsn";
+  # };
+  home.username = "mtnptrsn";
+  home.homeDirectory = "/Users/mtnptrsn";
+
   system.stateVersion = 5;
-
   services.nix-daemon.enable = true;
-
-  # The platform the configuration will be used on.
   nixpkgs.hostPlatform = "aarch64-darwin";
-
-  users.users.mtnptrsn = {
-    name = "mtnptrsn";
-    home = "/Users/mtnptrsn";
-  };
 }
