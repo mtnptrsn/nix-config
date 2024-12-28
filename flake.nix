444a902b4657e841d4dc587f597bc87123423f11
Mårten Pettersson <mtnptrsn@gmail.com>
2024-12-28 09:29:58 +0100

    iter

--- flake.nix
+++ flake.nix
@@ -2,18 +2,18 @@
   description = "mtnptrsn";
 
   inputs = {
-    nixpkgsLinux = {
+    nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
 
-    nixpkgsDarwin = {
-      url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
-    };
+    # nixpkgsDarwin = {
+    #   url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    # };
 
     home-manager = {
       url = "github:nix-community/home-manager";
       # Assume we use nixpkgs from Linux for Home Manager purposes across systems
-      inputs.nixpkgs.follows = "nixpkgsLinux";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
     nix-darwin = {
@@ -22,16 +22,15 @@
     };
   };
 
-  outputs = inputs@{ nixpkgsLinux, nixpkgsDarwin, home-manager, nix-darwin, ... }:
+  outputs = inputs@{ nixpkgs, nixpkgsDarwin, home-manager, nix-darwin, ... }:
     let
       linuxSystem = "x86_64-linux";
       darwinSystem = "aarch64-darwin";
-      pkgsLinux = nixpkgsLinux.legacyPackages.${linuxSystem};
-      pkgsDarwin = nixpkgsDarwin.legacyPackages.${darwinSystem};
+      pkgs = nixpkgs.legacyPackages.${linuxSystem};
     in
     {
       homeConfigurations."mtnptrsn-linux" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgsLinux;
+        inherit pkgs;
         modules = [ ./home-manager/home.nix ];
       };
 
