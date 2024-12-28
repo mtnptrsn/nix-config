97a0d45d03ea19133ccfef17b76f022c15542454
Mårten Pettersson <mtnptrsn@gmail.com>
2024-12-28 11:11:24 +0100

    iter

--- flake.nix
+++ flake.nix
@@ -11,32 +11,38 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ nixpkgs, home-manager, darwin, ... }: {
-    homeConfigurations = {
-      "mtnptrsn" = home-manager.lib.homeManagerConfiguration {
-        # system = "x86_64-linux";
-        # pkgs = nixpkgs.legacyPackages.${system};
+  outputs = inputs@{ nixpkgs, home-manager, darwin, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations = {
+        "mtnptrsn" = home-manager.lib.homeManagerConfiguration {
+          # system = "x86_64-linux";
+          # pkgs = nixpkgs.legacyPackages.${system};
 
-        inherit (nixpkgs.legacyPackages.x86_64-linux);
+          inherit pkgs;
 
-        system = "x86_64-linux";
-        modules = [ ./linux.nix ./home-manager/home.nix ];
+          system = "x86_64-linux";
+          modules = [ ./linux.nix ./home-manager/home.nix ];
+        };
+      };
+
+      darwinConfigurations = {
+        "mtnptrsn" = darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          modules = [
+            ./darwin.nix
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.mtnptrsn = import ./home-manager/home.nix;
+            }
+          ];
+        };
       };
     };
 
-    darwinConfigurations = {
-      "mtnptrsn" = darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
-        modules = [
-          ./darwin.nix
-          home-manager.darwinModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.mtnptrsn = import ./home-manager/home.nix;
-          }
-        ];
-      };
-    };
-  };
 }
