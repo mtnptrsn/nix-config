c973bf1fb6b4d3ac674d0fb2345729255033b7b5
Mårten Pettersson <mtnptrsn@gmail.com>
2024-12-28 12:32:41 +0100

    iter

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,8 +1,6 @@
 { pkgs, ... }:
 
 {
-  # home.username = "mtnptrsn";
-  # home.homeDirectory = "/home/mtnptrsn";
   home.stateVersion = "24.05";
   home.packages = with pkgs; [
     nodejs_20
@@ -18,22 +16,22 @@
   ];
   programs = {
     neovim.enable = true;
-    # ripgrep.enable = true;
-    # git.enable = true;
-    # zsh.enable = true;
-    # eza.enable = true;
-    # fzf.enable = true;
-    # zoxide.enable = true;
-    # tmux.enable = true;
-    # alacritty.enable = true;
-    # home-manager.enable = true;
+    ripgrep.enable = true;
+    git.enable = true;
+    zsh.enable = true;
+    eza.enable = true;
+    fzf.enable = true;
+    zoxide.enable = true;
+    tmux.enable = true;
+    alacritty.enable = true;
+    home-manager.enable = true;
   };
   imports = [
-    # ./modules/tmux.nix
-    # ./modules/zsh/zsh.nix
-    # ./modules/git.nix
+    ./modules/tmux.nix
+    ./modules/zsh/zsh.nix
+    ./modules/git.nix
     ./modules/neovim/neovim.nix
-    # ./modules/fzf.nix
-    # ./modules/alacritty/alacritty.nix
+    ./modules/fzf.nix
+    ./modules/alacritty/alacritty.nix
   ];
 }
